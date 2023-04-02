2864a0b2009ece11e309b80e7d0999080dd27dea
Cristian <>
2023-04-02 16:37:06 -0700

    change augment.py to overwrite shallower evals

--- tools/sqlite/augment.py
+++ tools/sqlite/augment.py
@@ -7,39 +7,25 @@ from dbutils.sqlite import SQLConn
 from tqdm import tqdm
 
 
-def augment_database(primary_db_file, secondary_db_file, batch_size=1000):
-    with SQLConn(primary_db_file) as primary_db, SQLConn(secondary_db_file) as secondary_db:
-        # Add depth, score, and has_evaluation columns to the primary database schema if they don't exist
-        primary_db.add_column_if_not_exists('position', 'depth', 'integer')
-        primary_db.add_column_if_not_exists('position', 'score', 'integer')
-        primary_db.add_column_if_not_exists('position', 'has_evaluation', 'integer')
+def augment_database(primary_db, secondary_db):
+    with SQLConn(primary_db) as primary_conn, SQLConn(secondary_db) as secondary_conn:
+        primary_conn.add_column_if_not_exists('position', 'depth', 'integer')
+        primary_conn.add_column_if_not_exists('position', 'score', 'integer')
+        primary_conn.add_column_if_not_exists('position', 'has_evaluation', 'integer')
 
-        # Iterate through rows of the secondary database using the cursor
-        secondary_db_cursor = secondary_db.exec('SELECT * FROM position')
-        secondary_db_row_count = secondary_db.row_count('position')
+        secondary_data = secondary_conn.exec('SELECT epd, depth, score FROM position')
+        for epd, depth, score in tqdm(secondary_data, total=secondary_conn.row_count('position')):
+            primary_row = primary_conn.exec('SELECT depth, has_evaluation FROM position WHERE epd=?', (epd,)).fetchone()
 
-        updates = []
-        for epd, depth, score in tqdm(secondary_db_cursor, total=secondary_db_row_count, desc='Augmenting primary database'):
-            updates.append((depth, score, 1, epd))
+            if primary_row:
+                primary_depth, has_evaluation = primary_row
+                if not has_evaluation or (depth > primary_depth):
+                    primary_conn.exec('UPDATE position SET depth=?, score=?, has_evaluation=1 WHERE epd=?', (depth, score, epd))
+            else:
+                print(f'No matching position found in primary database for EPD: {epd}')
 
-            if len(updates) >= batch_size:
-                primary_db.executemany('''
-                    UPDATE position
-                    SET depth=?, score=?, has_evaluation=?
-                    WHERE epd=?
-                ''', updates)
-                primary_db.commit()
-                updates = []
-
-        # Process any remaining updates
-        if updates:
-            primary_db.executemany('''
-                UPDATE position
-                SET depth=?, score=?, has_evaluation=?
-                WHERE epd=?
-            ''', updates)
-            primary_db.commit()
-
+        primary_conn.commit()
+        print(f'Augmented primary database {primary_db} with depth and score information from {secondary_db}')
 
 if __name__ == '__main__':
     class CustomFormatter(
@@ -52,12 +38,10 @@ if __name__ == '__main__':
         description='Augment the primary database with depth and score information from the secondary database.')
     parser.add_argument('primary_db', metavar='primary_db_file', type=str, help='Primary database file')
     parser.add_argument('secondary_db', metavar='secondary_db_file', type=str, help='Secondary database file containing depth and score information')
-    parser.add_argument('-b', '--batch_size', metavar='batch_size', type=int, default=1000, help='Number of updates to process in a batch (default: 1000)')
 
     args = parser.parse_args()
 
     try:
-        augment_database(args.primary_db, args.secondary_db, args.batch_size)
-        print(f'Augmented primary database {args.primary_db} with depth and score information from {args.secondary_db}')
+        augment_database(args.primary_db, args.secondary_db)
     except KeyboardInterrupt:
         pass
