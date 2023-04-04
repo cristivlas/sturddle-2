89b427c2d623c4a3a9cc1d821c10a6509c05a49d
Cristian <>
2023-04-03 23:15:06 -0700

    alternate conversion from array of int8_t to Vec16f

--- nnue.h
+++ nnue.h
@@ -173,12 +173,18 @@ namespace nnue
                     {
                         Vector vw;
                         vw.load(&wt[j][i]);
+                    #if 0
                         const Vector in(
                             input[i],   input[i+1], input[i+2], input[i+3],
                             input[i+4], input[i+5], input[i+6], input[i+7],
                             input[i+8], input[i+9], input[i+10],input[i+11],
                             input[i+12],input[i+13],input[i+14],input[i+15]);
-
+                    #else
+                        Vec16c v16c;
+                        v16c.load(&input[i]);
+                        Vec16i v16i = extend(extend(v16c));
+                        Vector in = to_float(v16i);
+                    #endif
                         int unroll_idx = (i - ii) / Vector::size();
                         sum_unrolled[unroll_idx] = mul_add(in, vw, sum_unrolled[unroll_idx]);
                     }
@@ -188,7 +194,6 @@ namespace nnue
 
                 for (int i = R; i != INPUTS; ++i)
                     output[j] += input[i] * wt[j][i];
-
             #endif
             }
         }
