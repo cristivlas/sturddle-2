efde98ba3bdcbf10c67a4c2f51d96b6843ea8d30
Cristian <>
2023-06-04 15:22:08 -0700

    pgnsplit: avoid memory allocation

--- tools/sqlite/pgnsplit.py
+++ tools/sqlite/pgnsplit.py
@@ -8,36 +8,43 @@ from tqdm import tqdm
 
 
 def split_pgn_file(input_file, output_prefix, n):
-    file_size = os.path.getsize(input_file)
-
-    with open(input_file, 'r', encoding='latin1') as pgn_file:
+    with open(input_file, 'rb') as pgn_file:
         mmapped_file = mmap.mmap(pgn_file.fileno(), 0, access=mmap.ACCESS_READ)
-        lines = mmapped_file.read().decode('latin1').splitlines()
-        pbar = tqdm(total=file_size, unit='B', unit_scale=True, desc='Reading file')
-        for line in lines:
-            delta = len(line.encode('latin1')) + 1
-            pbar.update(min(delta, file_size - pbar.n))
+        file_size = os.path.getsize(input_file)
+        event_indices = []
+
+        event_bytes = b'[Event '
+        pbar = tqdm(total=file_size, unit='B', unit_scale=True, desc='Counting games')
+        pos = 0
+        while pos < len(mmapped_file):
+            new_pos = mmapped_file.find(event_bytes, pos)
+            if new_pos == -1:
+                break
+            event_indices.append(new_pos)
+            pbar.update(new_pos - pos)
+            pos = new_pos + len(event_bytes)
+        pbar.update(file_size - pos)
         pbar.close()
 
-    event_indices = [i for i, line in tqdm(enumerate(lines), total=len(lines), unit='game', desc='Counting games') if line.startswith('[Event ')]
-    num_games = len(event_indices)
-    games_per_file = num_games // n
+        num_games = len(event_indices)
+        games_per_file = num_games // n
 
-    for i in tqdm(range(n), total=n, unit='file', desc='Splitting files'):
-        start_index = i * games_per_file
-        if i == n - 1:
-            end_index = num_games
-        else:
-            end_index = start_index + games_per_file
-        start_line = event_indices[start_index]
-        end_line = event_indices[end_index] if end_index < num_games else len(lines)
+        pbar = tqdm(total=num_games, unit='game', desc='Splitting file')
+        for i in range(n):
+            start_index = i * games_per_file
+            if i == n - 1:
+                end_index = num_games
+            else:
+                end_index = start_index + games_per_file
+            start_line = event_indices[start_index]
+            end_line = event_indices[end_index] if end_index < num_games else len(mmapped_file)
 
-        output_file = f'{output_prefix}_{i+1:02d}.pgn'
-        with open(output_file, 'w') as out_file:
-            for line in lines[start_line:end_line]:
-                out_file.write(line)
-    print(f'Successfully split PGN file into {n} parts.')
-
+            output_file = f'{output_prefix}_{i+1:02d}.pgn'
+            with open(output_file, 'wb') as out_file:
+                out_file.write(mmapped_file[start_line:end_line])
+            pbar.update(end_index - start_index)
+        pbar.close()
+    print('Successfully split PGN file into {} parts.'.format(n))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Split PGN files')
@@ -47,7 +54,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if not os.path.isfile(args.input_file):
-        print(f'Input file {args.input_file} does not exist.')
+        print('Input file {} does not exist.'.format(args.input_file))
         sys.exit(1)
 
     if args.n <= 0:
