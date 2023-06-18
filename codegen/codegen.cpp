43f9056b022f6f5e0d427ac7f766a5c60f87a7d7
Cristian <>
2023-06-17 17:14:26 -0700

    reorder attack tables

--- codegen/codegen.cpp
+++ codegen/codegen.cpp
@@ -29,9 +29,9 @@ int main(int argc, const char* argv[])
     chess::_init();
 
     if (builder.build_group<AttacksType::Diag>({-9, -7, 7, 9})
+     && builder.build_group<AttacksType::Rook>({-8, -1, 1, 8})
      && builder.build_group<AttacksType::File>({-8, 8})
-     && builder.build_group<AttacksType::Rank>({-1, 1})
-     && builder.build_group<AttacksType::Rook>({-8, -1, 1, 8}))
+     && builder.build_group<AttacksType::Rank>({-1, 1}))
     {
         builder.write(std::cout);
         return 0;
