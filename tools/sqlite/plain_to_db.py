4540db5da8545ba7b36da7593d62071c3f956697
Cristian <>
2023-06-20 22:38:22 -0700

    quick tool to import plain training data

---
+++ tools/sqlite/plain_to_db.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import argparse
+import mmap
+
+from dbutils.sqlite import SQLConn
+from tqdm import tqdm
+
+
+class SQLConnExtended(SQLConn):
+    def create_table_if_not_exists(self):
+        _create_table = '''
+        CREATE TABLE IF NOT EXISTS position(
+            epd text PRIMARY KEY,
+            depth integer,
+            score integer
+        )'''
+        self.exec(_create_table)
+        self._conn.commit()
+
+
+def process_file(file_path, db_path):
+    with SQLConnExtended(db_path) as conn:
+        conn.create_table_if_not_exists()
+
+        with open(file_path, 'r') as file:
+            map_file = mmap.mmap(file.fileno(), length=0, access=mmap.ACCESS_READ)
+
+        record_count = 0
+        for line in iter(map_file.readline, b''):
+            if line.startswith(b'e'):
+                record_count += 1
+
+        map_file.seek(0)  # Reset the mmap object to start of file
+
+        progress_bar = tqdm(total=record_count)
+
+        fen = b''
+        depth = 0
+        score = 0
+
+        for line in iter(map_file.readline, b''):
+            if line.startswith(b'fen'):
+                fen = line[4:].strip()
+            elif line.startswith(b'score'):
+                score = int(line[6:])
+            elif line.startswith(b'ply'):
+                depth = int(line[4:])
+            elif line.startswith(b'e'):
+                conn.exec('INSERT OR IGNORE INTO position VALUES (?, ?, ?)', (fen.decode('utf-8'), depth, score))
+                conn.commit()
+                progress_bar.update()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert text file to SQLite3 database.')
+    parser.add_argument('input', help='Input file path')
+    parser.add_argument('-o', '--output', help='Output SQLite3 database path')
+    args = parser.parse_args()
+
+    process_file(args.input, args.output)
