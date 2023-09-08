39d4278331e77e9e721e1d265b1f87fce30cb604
Cristian <>
2023-09-08 03:03:16 -0700

    small arm change

--- armvector.h
+++ armvector.h
@@ -128,7 +128,8 @@ INLINE Vec4f operator * (const Vec4f& a, const Vec4f& b)
 
 INLINE Vec4f mul_add(const Vec4f& a, const Vec4f& b, const Vec4f& c)
 {
-    return vfmaq_f32(c, a, b);
+    //return vfmaq_f32(c, a, b);
+    return vmlaq_f32(c, a, b);
 }
 
 INLINE Vec4f max(const Vec4f& a, const Vec4f& b)
