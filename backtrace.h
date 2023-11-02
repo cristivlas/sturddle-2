9a2574ba60a1672f0c5d40414ff5b2e631851a20
Cristian <>
2023-11-01 22:30:17 -0700

    relax Android API requirement

--- backtrace.h
+++ backtrace.h
@@ -1,7 +1,4 @@
 #pragma once
-#if __ANDROID__
-static_assert(__ANDROID_API__ >= 33);
-#endif /* __ANDROID__ */
 
 #if __APPLE__ || __linux__
 #include <cxxabi.h>
@@ -12,6 +9,7 @@ static_assert(__ANDROID_API__ >= 33);
 template <int MAX_FRAMES = 256>
 void dump_backtrace(std::ostream &out)
 {
+#if !__ANDROID__ || __ANDROID_API__ >= 33
     void *frames[MAX_FRAMES] = {0};
 
     int num_frames = backtrace(frames, MAX_FRAMES);
@@ -30,6 +28,7 @@ void dump_backtrace(std::ostream &out)
     {
         out << symbols[i] << std::endl;
     }
+#endif /* !__ANDROID__ || __ANDROID_API__ >= 33 */
 }
 #else
 template <int MAX_FRAMES = 256>
