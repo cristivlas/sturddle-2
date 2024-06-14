340c25a862c4b06fdb783a7bd8ac311c8c9fbc3e
Cristian Vlasceanu <cvlasceanu@cvlasce-ltmbz4b.internal.salesforce.com>
2024-06-14 15:58:11 -0700

    experiment native Vec16s on NEON

--- armvector.h
+++ armvector.h
@@ -227,6 +227,7 @@ INLINE Vec8f mul_add(const Vec8f& a, const Vec8f& b, const Vec8f& c)
 #endif /* __ARM_FEATURE_FP16_VECTOR_ARITHMETIC */
 
 
+#if 0 /* Emulate with SIMDE */
 class Vec16s
 {
     __m256i ymm;
@@ -289,6 +290,76 @@ INLINE Vec16s operator * (const Vec16s& a, const Vec16s& b)
 {
     return _mm256_mullo_epi16(a, b);
 }
+#else /* NEON */
+
+class Vec16s
+{
+    int16x8x2_t _data;
+
+public:
+    static constexpr size_t size() { return 16; }
+
+    Vec16s() = default;
+
+    Vec16s(int16_t i) : _data{vdupq_n_s16(i), vdupq_n_s16(i)} {}
+
+    Vec16s(int16x8x2_t data) : _data(data) {}
+
+    Vec16s(int16_t i0, int16_t i1, int16_t i2,  int16_t i3,  int16_t i4,  int16_t i5,  int16_t i6,  int16_t i7,
+           int16_t i8, int16_t i9, int16_t i10, int16_t i11, int16_t i12, int16_t i13, int16_t i14, int16_t i15)
+    {
+        int16_t values[16] = {i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15};
+        _data = {vld1q_s16(&values[0]), vld1q_s16(&values[8])};
+    }
+
+    INLINE int16x8_t get_low() const { return _data.val[0]; }
+    INLINE int16x8_t get_high() const { return _data.val[1]; }
+
+    INLINE void load(const int16_t* p) { _data = vld1q_s16_x2(p); }
+    INLINE void load_a(const int16_t* p) { _data = vld1q_s16_x2(p); }
+    INLINE void store_a(int16_t* p) const { vst1q_s16_x2(p, _data); }
+};
+
+INLINE int16_t horizontal_add(const Vec16s& a)
+{
+    int16x8_t sum1 = vpaddq_s16(a.get_low(), a.get_high());
+    int16x4_t sum2 = vadd_s16(vget_low_s16(sum1), vget_high_s16(sum1));
+    int16x4_t sum3 = vpadd_s16(sum2, sum2);
+    return vget_lane_s16(sum3, 0) + vget_lane_s16(sum3, 1);
+}
+
+INLINE bool horizontal_or(const Vec16s& a)
+{
+    int16x8_t or1 = vorrq_s16(a.get_low(), a.get_high());
+    int16x4_t or2 = vorr_s16(vget_low_s16(or1), vget_high_s16(or1));
+    int16x4_t or3 = vpmax_s16(or2, or2);
+    return vget_lane_s16(or3, 0) | vget_lane_s16(or3, 1);
+}
+
+INLINE Vec16s operator + (const Vec16s& a, const Vec16s& b)
+{
+    int16x8x2_t result;
+    result.val[0] = vaddq_s16(a.get_low(), b.get_low());
+    result.val[1] = vaddq_s16(a.get_high(), b.get_high());
+    return result;
+}
+
+INLINE Vec16s operator - (const Vec16s& a, const Vec16s& b)
+{
+    int16x8x2_t result;
+    result.val[0] = vsubq_s16(a.get_low(), b.get_low());
+    result.val[1] = vsubq_s16(a.get_high(), b.get_high());
+    return result;
+}
+
+INLINE Vec16s operator * (const Vec16s& a, const Vec16s& b)
+{
+    int16x8x2_t result;
+    result.val[0] = vmulq_s16(a.get_low(), b.get_low());
+    result.val[1] = vmulq_s16(a.get_high(), b.get_high());
+    return result;
+}
+#endif
 
 template <typename V> INLINE V& operator += (V& a, V b)
 {
