bbe8693e8a7856484ef10e5104cb02429d48e8e2
Cristian <cristi.vlasceanu@gmail.com>
2024-06-28 10:11:36 -0700

    merge dev/dev-tune changes

--- tools/make-win.bat
+++ tools/make-win.bat
@@ -1,5 +1,7 @@
 python setup.py clean --all
-REM set CL_EXE=C:\Program Files\LLVM\bin\clang-cl.exe
-set CFLAGS="-march=native %CFLAGS%"
+@setlocal
+set CL_EXE=C:\Program Files\LLVM\bin\clang-cl.exe
+set CXXFLAGS=-march=native
 set NATIVE_UCI=1
 python setup.py build_ext --inplace
+@endlocal
