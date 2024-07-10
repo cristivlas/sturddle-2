e50ab5ae47bfe556365c8ae9e7a173d3076ef6da
Cristian <cristi.vlasceanu@gmail.com>
2024-07-09 19:51:51 -0700

    pass CXXFLAGS to make-win.bat

--- tools/make-win.bat
+++ tools/make-win.bat
@@ -1,7 +1,7 @@
 python setup.py clean --all
 @setlocal
 set CL_EXE=C:\Program Files\LLVM\bin\clang-cl.exe
-set CXXFLAGS=-march=native
+set CXXFLAGS=-march=native %CXXFLAGS%
 set NATIVE_UCI=1
 python setup.py build_ext --inplace
 @endlocal
