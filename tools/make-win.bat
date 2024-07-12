99c110641e5a466c01dd9666b2085182ad3f7c30
Cristian <cristi.vlasceanu@gmail.com>
2024-07-12 16:31:19 -0700

    remove -march=native from make-win.bat

--- tools/make-win.bat
+++ tools/make-win.bat
@@ -1,7 +1,6 @@
 python setup.py clean --all
 @setlocal
 set CL_EXE=C:\Program Files\LLVM\bin\clang-cl.exe
-set CXXFLAGS=-march=native %CXXFLAGS%
 set NATIVE_UCI=1
 python setup.py build_ext --inplace
 @endlocal
