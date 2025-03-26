15a60339e1f6d5c3fe5549fc74c28615bdaee40f
cristivlas <cristi.vlasceanu@gmail.com>
2025-03-26 10:02:02 -0700

    sync up changes from 2.02.rc3

--- __init__.pyx
+++ __init__.pyx
@@ -1,7 +1,7 @@
 # distutils: language = c++
 # cython: language_level = 3
 """
-Sturddle Chess Engine (c) 2022, 2023, 2024 Cristian Vlasceanu.
+Sturddle Chess Engine (c) 2022 - 2025 Cristian Vlasceanu.
 -------------------------------------------------------------------------
 
 This program is free software: you can redistribute it and/or modify
