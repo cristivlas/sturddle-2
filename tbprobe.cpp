013179928a0c60793f5a2cb3364c99f015c1d680
cristivlas <cristi.vlasceanu@gmail.com>
2025-11-24 18:16:40 -0800

    typo

--- tbprobe.cpp
+++ tbprobe.cpp
@@ -1,7 +1,7 @@
 #include "config.h"
 #if USE_ENDTABLES
 #define atomic_init(x,y) (x)->store(y)
-#if _WIN32 and !defined(INFINITE)
+#if _WIN32 && !defined(INFINITE)
     constexpr DWORD INFINITE = 0xFFFFFFFF;
 #endif
 #include "tbprobe.c"
